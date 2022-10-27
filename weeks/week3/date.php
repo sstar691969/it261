9dc2c60549572e9fe9ad23c30c7ba8afcd804c16
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-10-26 17:37:26 -0700

    first commit

---
+++ weeks/week3/date.php
@@ -0,0 +1,95 @@
+<?php
+// date function and if statement
+echo date ('Y');
+echo '<br>';
+echo date('l,F j, Y h:i: A');
+
+echo '<br>';
+date_default_timezone_set('America/Los_Angeles');
+
+
+echo '<br>';
+echo date('l,F j, Y h:i: A');
+
+
+
+
+echo '<br>';
+
+$name = 'Olga';
+$our_time= date('H:i A');
+
+echo '<br>';
+echo $our_time;
+
+
+//  the  logic behind this is, If the time is less or equal to 11, then it is morning.
+//  if the time is less or equal to 17, which equal 5, then it is afternoon.
+//  now we will have an else, that will mean it is evening!
+
+if($our_time <= 8) {
+echo '<h2 style="color:yellow;">Good Morning, '.$name.'</h2>
+<img src="../../images/sunny.jpeg" alt="sun">
+<p>It\'s time to get up</p>';
+
+
+} elseif ($our_time <= 17) {
+echo '<h2 style="color:green;>Good Afternoon, '.$name.'</h2>';
+
+
+} else {
+echo '< style="color:blue;>Good Evening! '.$name.'</h2>';
+
+}
+
+
+
+
+
+
+echo '<br>';
+
+
+
+echo '<br>';
+
+
+
+
+
+echo '<br>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
