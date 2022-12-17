8525bd2f5970ac2ece16c6f940b34a52e83b0119
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-12-17 13:34:15 -0800

    commit week 9
    Im having issues with server.php 
    server.php is not loading up on web browser. why?

---
+++ week9/creditials.php
@@ -0,0 +1,14 @@
+
+<?php
+
+
+// willanderson_codes - name of my database
+// star_691969user - 2nd user name
+
+
+define('DB_NAME', 'willanderson_codes');
+define('DB_USER', 'sstar_691969');
+define('DB_PASSWORD', '2100Anderson');
+define('DB_HOST', 'mysql.willandersoncodes.com');
+// on Dreamhost, you may have to use a different value for your host. Note: Host:mysql.willandersoncodes.com
+
