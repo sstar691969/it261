bb15e7f8630591e1137d8999c44c345393206eda
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-03-15 01:59:56 -0700

    new commit
    creditials.php

---
+++ creditials.php
@@ -0,0 +1,17 @@
+
+<?php
+
+
+
+
+// willanderson_codes - name of my database
+// Sstar_691969 - first user name
+// Star_691969user - 2nd user
+
+
+define('DB_NAME', 'willanderson_codes');
+define('DB_USER', 'sstar_691969');
+define('DB_PASSWORD', '2100Anderson');
+define('DB_HOST', 'mysql.willandersoncodes.com');
+// on Dreamhost, you may have to use a different value for your host. Note: Host:mysql.willandersoncodes.com
+
